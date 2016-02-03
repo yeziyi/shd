1088a24cef987a0691f9d009729d4caeae6d80f3
yijiajia1988 <yijiajia1988@gmail.com>
2016-02-03 18:26:19 +0800

    提交

--- src/com/smartapp/shades/view/Green.java
+++ src/com/smartapp/shades/view/Green.java
@@ -1,8 +1,8 @@
 package com.smartapp.shades.view;
 
 public class Green extends Color {
-	private int[] array = new int[] { 0xFFcccccc, 0xFFaaaaaa, 0xFF888888,
-			0xFF555555, 0xFF444444, 0xFF000000 };
+	private int[] array = new int[] { 0xFFdddddd, 0xFFaaaaaa, 0xFF999999,
+			0xFF0000FF, 0xFF222222, 0xFFFF0000 };
 
 	@Override
 	public int[] getColorList() {
